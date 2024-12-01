b59ee406b708971287f2e8f6e091e1fec68ee439
Luis Miguel Morales <lmoralesn@uni.pe>
2024-11-30 20:03:53 -0500

    Finish merge

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/components/matchCard.js
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -39,46 +38,3 @@ export default function MatchCard({ match, setFormOpen }) {
     </Card>
   );
 }
-=======
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-export default function MatchCard({ match }) {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="soccer-field-1.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {match.date}
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          {match.description}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {match.location}
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>11:00 AM</Typography>
-        <Typography variant="body2">
-          Equipos de {match.teamSize} vs {match.teamSize}
-          <br />
-          {`s/. ${match.price}`}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Jugar</Button>
-        <Button size="small">Más detalles</Button>
-      </CardActions>
-    </Card>
-  );
-}
->>>>>>> 8edfd8ade2682d2cb150031709bb5ccccfb98410:src/components/MatchCard.js
