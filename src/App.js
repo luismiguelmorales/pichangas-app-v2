8da2e81d7901b15d6f4a16f45fbe066e6513a7f3
Luis Miguel Morales <lmoralesn@uni.pe>
2024-12-13 01:55:10 -0500

    Add style to button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+import ButtonUsage from './components/Button';
 
 export default function App() {
   return (
     <div>
       <div className='rect'>RECTANGLE</div>
       <h1>Welcome to my app</h1>
-      <MyButton />
+      <ButtonUsage />
     </div>
   );
 }
