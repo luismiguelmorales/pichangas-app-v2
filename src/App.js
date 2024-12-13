9dc37fb886c8b5064d990a72fc30c52ac3971317
Luis Miguel Morales <lmoralesn@uni.pe>
2024-12-13 10:06:02 -0500

    Add MUI components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
-import ButtonUsage from './components/Button';
+import * as React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import playersPerTeam from './playersPerTeam';
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 export default function App() {
+  const [alignment, setAlignment] = React.useState('web');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <div>
-      <div className='rect'>RECTANGLE</div>
-      <h1>Welcome to my app</h1>
-      <ButtonUsage />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DatePicker']}>
+          <DatePicker label="Escoja una fecha" />
+        </DemoContainer>
+      </LocalizationProvider>
+      <Switch {...label} />
+      <ToggleButtonGroup
+        color="primary"
+        value={alignment}
+        exclusive
+        onChange={handleChange}
+        aria-label="Platform"
+      >
+        <ToggleButton value="1">1km</ToggleButton>
+        <ToggleButton value="3">3km</ToggleButton>
+        <ToggleButton value="5">5km</ToggleButton>
+        <ToggleButton value="10">10km</ToggleButton>
+        <ToggleButton value="30">30km</ToggleButton>
+      </ToggleButtonGroup>
+      <Autocomplete
+        disablePortal
+        options={playersPerTeam}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="" />}
+      />
+      <Button variant="outlined">Aplicar filtros</Button>
     </div>
   );
 }
---
+++ src/playersPerTeam.js
@@ -0,0 +1,13 @@
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const playersPerTeam = [
+    { label: '5 por equipo', year: 1994 },
+    { label: '6 por equipo', year: 1972 },
+    { label: '7 por equipo', year: 1972 },
+    { label: '8 por equipo', year: 1972 },
+    { label: '9 por equipo', year: 1972 },
+    { label: '10 por equipo', year: 1972 },
+    { label: '11 por equipo', year: 1972 },
+  ];
+  
+  export default playersPerTeam;
+  
